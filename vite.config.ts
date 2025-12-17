9fd9c3cf8a6c9b3fbdf512a704b90aa3586eddd7
ArtefactX1 <123149085+ArtefactX1@users.noreply.github.com>
2025-12-17 13:05:27 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -51,7 +51,22 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              if (id.includes('react')) {
+                return 'vendor-react';
+              }
+              if (id.includes('recharts')) {
+                  return 'vendor-recharts';
+              }
+              return 'vendor';
+            }
+          }
+        }
+      }
     },
     server: {
       port: 3000,
